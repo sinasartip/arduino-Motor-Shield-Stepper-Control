ebfbff7f3f49e2e11c5f90537b7b0e8fa83a3396
sinasartip <ssartip@gmail.com>
2020-10-09 21:32:30 -0400

    highly coupled control loop

--- main.ino
+++ main.ino
@@ -1,8 +1,21 @@
 
-void setup() {
+int incomingByte = 0;
+int motor_speed = 0;
 
+void setup() {
+    UI.being();
+    Motor.begin()
+    Serial.begin(9600);
 }
 
 void loop(){
-    
+    //read in serial data 
+    if (Serial.available() > 0){
+        incomingByte = Serial.read();
+        Serial.println(incomingByte);
+        if (incomingByte <= 100 && incomingByte >= -100){
+            motor_speed = UI.motor_input(incomingByte);
+            Motor.move(motor_speed);
+        }
+    }
 }
