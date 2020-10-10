5803b8850cf89cbc139dbc830d6d74a502fcd156
sinasartip <ssartip@gmail.com>
2020-10-09 23:57:05 -0400

    code needs testing

--- main.ino
+++ main.ino
@@ -13,12 +13,16 @@ void setup() {
 
 void loop(){
     //read in serial data 
+    Serial.println("in loop");
     if (Serial.available() > 0){
-        
+        Serial.println("reading byte");
         incomingByte = Serial.read();
         Serial.println(incomingByte);
         
         motor_speed = UI_MapMotorSpeed(incomingByte);
+        Serial.println("mapped speed");
         Motor_move(motor_speed);
+        Serial.println("end of control!");
     }
+    delay(20);
 }
