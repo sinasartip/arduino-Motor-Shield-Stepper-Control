1b18504ed829bd3aab6d805d4702e446846c6ebf
sinasartip <ssartip@gmail.com>
2020-10-12 02:27:35 -0400

    added motor design

--- main.ino
+++ main.ino
@@ -1,31 +1,48 @@
 void welcomeMessage();
-void serial_write(String);
+void serial_write(String[],int);
+int readin_speed();
+void motor_move(int);
 
-int main(){
-    //enter setup values
+ int speed;
+
+void setup(){
+   
     welcomeMessage();
+}
 
+void loop(){
 
+    if(Serial.available() > 0)
+        speed = readin_speed();
+        motor_move(speed);
+        Serial.println(speed);
 
-
-
-    bool control_loop_isActive = true;
-    while(control_loop_isActive){
-
-    }
-
-    return 0;
 }
 
 void welcomeMessage(){
+    int number_of_message = 2;
+    String welcome[] = {"You are in stepper_speed_control.",
+                        "send speed as a percent."};
     
-    String welcome = "You are in stepper_speed_control./nsend speed percent.0";
-    serial_write(welcome);
+    serial_write(welcome,number_of_message);
 }
 
-void serial_write(String message){    
+int readin_speed(){
+    char incomingByte;
+    incomingByte = Serial.read();
+
+    return(atoi(&incomingByte));
+
+
+}
+
+void motor_move(int speed){
     
-    Serial.begin(9600); //Baud-Rate
-    Serial.println(message);
+}
+
+void serial_write(String message[],int size){    
+    Serial.begin(115200); //Baud-Rate
+    for(int i=0;i < size; i++)
+        Serial.println(message[i]);
 
 }
